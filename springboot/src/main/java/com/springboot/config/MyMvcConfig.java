da8314f0e831c2ce817c79c2e7cbe57a8b51afdd
daixiongxing <daixiongxing@youzan.com>
2021-06-21 23:05:03 +0800

    wq2

---
+++ springboot/src/main/java/com/springboot/config/MyMvcConfig.java
@@ -0,0 +1,14 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author daixiongxing
+ * @since 2021-06-21 下午10:32
+ */
+//扩展 springmvc
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+}
