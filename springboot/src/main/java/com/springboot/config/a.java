37cb8b62af85d4bb856f1ae2251628190765fb7f
13403080864 <480524962@qq.com>
2021-06-22 09:39:07 +0800

    622

---
+++ springboot/src/main/java/com/springboot/config/a.java
@@ -0,0 +1,15 @@
+package com.springboot.config;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+/**
+ * @author
+ * @since 2021-06-22 09:33
+ */
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("qe");
+    }
+
+}
